72297616c4195cb3abe06dd836ee17316ff1022c
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-03 20:52:37 +0530

    :sparkles: add new resolvers Todo

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,34 @@ async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
+            type User{
+                id:ID!
+                name:String!
+                username:String!
+                email:String!
+                phone:String!
+                website: String!
+            }
             type Todo{
                 id: ID!
                 title: String!
                 completed: Boolean
+                user: User
             }
             type Query{
                 getTodos:[Todo]
+                getAllUsers: [User]
+                getUser(id: ID!):User
             }
         `,
         resolvers: {
+            Todo: {
+                user: async (todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.id}`)).data,
+            },
             Query: {
-                getTodos: async () => (await axios.get("https://jsonplaceholder.typicode.com/todos")).data
+                getTodos: async () => (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+                getAllUsers: async () => (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+                getUser: async (parent, { id }) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data,
             }
         }
     });
