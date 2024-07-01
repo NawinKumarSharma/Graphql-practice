fbd70f5ef09cec9dfca8c4d5b1651373ddd00d4d
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-07-01 22:07:23 +0545

    :sparkles: database connected

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,17 +5,14 @@ import morgan from 'morgan'
 import dotenv from 'dotenv'
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone';
-
+import {schema} from "./graphql/schema/schema.js"
 dotenv.config({ path: './.env', });
 
 export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
 const port = Number(process.env.PORT) || 3000;
 
 const server = new ApolloServer({
-  typeDefs: `type Query {
-    hello: String,
-    hi: String
-  }`,
+  typeDefs: schema,
   resolvers: {
     Query: {
       hello: () => "hello world",
@@ -34,6 +31,10 @@ startStandaloneServer(server, {
   console.log("error: ", e);
 
 })
+
+
+
+
 // const app = express();
 
 
