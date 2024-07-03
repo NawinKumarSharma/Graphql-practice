a21e748de3ae103a958ffd63c42ca36212c30d2b
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-07-03 07:27:58 +0545

    :sparkles: add docker compose and prisma/migrations

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,71 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import { errorMiddleware } from './middlewares/error.js'
-import morgan from 'morgan'
-import dotenv from 'dotenv'
-import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { schema } from "./graphql/schema/schema.js"
-import connectDB from './db/index.js'
-import { User } from './models/user.model.js'
-import { getAllUsers } from './controllers/user.js'
-dotenv.config({ path: './.env', });
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 
-export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
-const port = Number(process.env.PORT) || 3000;
-connectDB()
-  .catch((err) => {
-    console.log('MONGODB CONNECTION FAILED', err);
-  })
-const server = new ApolloServer({
-  typeDefs: schema,
-  resolvers: {
-    Query: {
-      hello: () => "hello world",
-      hi: () => "hi there",
-      users: getAllUsers,
+async function init() {
+  const app = express();
+  const PORT = Number(process.env.PORT) || 8000;
+
+  app.use(express.json());
+
+  // Create Graphql Server
+  const gqlServer = new ApolloServer({
+    typeDefs: `
+        type Query {
+            hello: String
+            say(name: String): String
+        }
+    `, // Schema
+    resolvers: {
+      Query: {
+        hello: () => `Hey there, I am a graphql server`,
+        say: (_, { name }: { name: string }) => `Hey ${name}, How are you?`,
+      },
     },
-  },
-});
-startStandaloneServer(server, {
-  listen: {
-    port,
-  }
-}).then(() => {
-  console.log('Server is working on  Port:' + port + ' in ' + envMode + ' Mode.');
+  });
 
-}).catch(e => {
-  console.log("error: ", e);
+  // Start the gql server
+  await gqlServer.start();
 
-})
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
 
+  app.use("/graphql", expressMiddleware(gqlServer));
 
+  app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
+}
 
-
-// const app = express();
-
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ origin: ' * ', credentials: true }));
-// app.use(morgan('dev'))
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-// });
-
-// // your routes here
-
-
-// app.get("*", (req, res) => {
-//   res.status(404).json({
-//     success: false,
-//     message: 'Page not found'
-//   });
-// });
-
-// app.use(errorMiddleware);
-
-
-// app.listen(port, () => console.log('Server is working on Port:' + port + ' in ' + envMode + ' Mode.'));
+init();
--- server/src/controllers/user.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../models/user.model.js";
-
-export const getAllUsers =  async()=>{
-        const users = await User.find();
-        console.log(users);
-        return users;
-      }
-    
